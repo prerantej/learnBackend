859d1d49df9ffe0f471c34844c2d5d39d2d7dc15
prerantej <prerantejkvr@gmail.com>
2024-06-28 00:52:03 +0530

    qr generator created

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require('sillyname');
+
+// import generateName from "sillyname";
+// console.log('My name is' + sillyName);
+// var sillyName = generateName();
+
+import superheroes from 'superheroes';
+
+ 
+var mySuperHeroName = superheroes.random();
+console.log(mySuperHeroName);
